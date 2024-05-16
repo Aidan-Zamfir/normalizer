d59763d4877f2a2e5945dbbdc05807ab086be43a
Aidan-Z <aidanzamfir@gmail.com>
2024-05-16 09:56:55 -0300

    init

---
+++ data/minmax.go
@@ -0,0 +1,25 @@
+package data
+
+
+
+func Minimum(arr []int) int {
+	min := arr[0]
+	for _, i := range arr {
+		if (i < min) {
+			min = i
+		}
+	}
+	return min
+}
+
+func Maximum(arr []int) int{
+	max := arr[0]
+	for _, i := range arr {
+		if (i > max) {
+			max = i
+		}
+	}
+	return max
+}
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aidan-Zamfir/normalizer
+
+go 1.22.2
