fdcc87eb2e314b5734fe021f42ac996555908ba1
Aidan-Z <aidanzamfir@gmail.com>
2024-05-25 07:19:50 -0300

    main

--- main.go
+++ main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/Aidan-Zamfir/normalizer/csvData"
 	"github.com/Aidan-Zamfir/normalizer/data"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
+
+
 
 func main() {
 
 	var filepath string
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 
 
 	fmt.Print("Enter file path: ") //(testdata.csv)
@@ -24,6 +21,5 @@ func main() {
 	fmt.Println(x, "<-- columns")
 
 	data.Caller() //input Data struct
-	
 	
 }
