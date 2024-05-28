537d12f7f86be58d54d75b7e43b42c04b40b099a
Aidan-Z <aidanzamfir@gmail.com>
2024-05-28 18:02:40 -0300

    notes and cleaned up code

--- main.go
+++ main.go
@@ -4,17 +4,31 @@ import (
 	"github.com/Aidan-Zamfir/normalizer/cli"
 )
 
-
 func main() {
 	cli.Execute()
 
 }
+
 // >>> go run . stand testdata.csv
 
 // main -> cli/root.go -> execute()  				WORKS
 // execute() -> init() 								WORKS
 // init() -> normalizeCmd() | standardiseCmd() 		WORKS
-// -> csvData/parser.go -> getCSVData(FILEPATH)		WORKS
-// parrses csv file -> returns struct (d)			WORKS
-// -> data/equations -> standardise(d)				  /
 
+// NM:
+// -> csvData/parser.go -> getCSVData(FILEPATH)		WORKS
+// •parrses csv file -> returns array struct (d)	WORKS
+// -> data/equations -> minmax(d)				  	  /
+// minmac(d) -> minimum(d)						 	  /
+//			 -> maximum(d)			  				  /
+// •normalize data -> retuns []float64{}			  /
+// NEEDS TO RETURN DATA AND CONVERT TO CSV			 (?)
+
+// STAND:
+// -> csvData/parser.go -> getCSVData(FILEPATH)		WORKS
+// •parrses csv file -> returns array struct (d)	WORKS
+// -> data/equations -> Standardise(d)				  /
+// Standardise(d) -> mean(d)						  /
+//				  -> standardDeviation(d)			  /
+// •standardises data -> retuns []float64{}			  /
+// NEEDS TO RETURN DATA AND CONVERT TO CSV			 (?)
