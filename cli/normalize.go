57e9e8c6d43dd1b70c23de7175b5879f0fa2f27f
Aidan-Z <aidanzamfir@gmail.com>
2024-05-31 11:31:08 -0300

    clean

--- cli/normalize.go
+++ cli/normalize.go
@@ -9,24 +9,24 @@ import (
 )
 
 var normalizeCmd = &cobra.Command{
-	Use: "nm", 
+	Use:   "nm",
 	Short: "Will return normalized values as X file form", //decide
-	Args: cobra.MinimumNArgs(1),
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		d, err := csvData.GetCSVData(args[0])
 		if err != nil {
-				log.Fatal(err)
-			}
+			log.Fatal(err)
+		}
 
 		//Currently only passing in one slice of flt64 (1 column) -> not entire dataset
-		
+
 		result := data.MinMax(d.First)
-		log.Println(result, "<- data std")
+		log.Println(result)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(normalizeCmd)
 	//need to add file path functionality
-	// normalizeCmd.Flags().StringVarP(&exportFilePath, "export", "e", "Export to file ->") 
+	// normalizeCmd.Flags().StringVarP(&exportFilePath, "export", "e", "Export to file ->")
 }
--- main.go
+++ main.go
@@ -10,25 +10,3 @@ func main() {
 }
 
 // >>> go run . stand testdata.csv
-
-// main -> cli/root.go -> execute()  				WORKS
-// execute() -> init() 								WORKS
-// init() -> normalizeCmd() | standardiseCmd() 		WORKS
-
-// NM:
-// -> csvData/parser.go -> getCSVData(FILEPATH)		WORKS
-// •parrses csv file -> returns array struct (d)	WORKS
-// -> data/equations -> minmax(d)				  	  /
-// minmac(d) -> minimum(d)						 	  /
-//			 -> maximum(d)			  				  /
-// •normalize data -> retuns []float64{}			  /
-// NEEDS TO RETURN DATA AND CONVERT TO CSV			 (?)
-
-// STAND:
-// -> csvData/parser.go -> getCSVData(FILEPATH)		WORKS
-// •parrses csv file -> returns array struct (d)	WORKS
-// -> data/equations -> Standardise(d)				  /
-// Standardise(d) -> mean(d)						  /
-//				  -> standardDeviation(d)			  /
-// •standardises data -> retuns []float64{}			  /
-// NEEDS TO RETURN DATA AND CONVERT TO CSV			 (?)
