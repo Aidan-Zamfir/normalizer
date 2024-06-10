d92c53b33d94772f66b5bcf0cf838555cd4ed75b
Aidan-Z <aidanzamfir@gmail.com>
2024-06-10 10:36:20 -0300

    removed unused code

--- csvData/toCSV.go
+++ csvData/toCSV.go
@@ -6,58 +6,6 @@ import (
 	"strconv"
 )
 
-// TO DO:
-// Re-insert column names (get from parser?)
-
-// t int,
-//func ToCSV(head [][]string, arr [][]float64, filePath string) error {
-//	//var filepath string
-//
-//	if filePath != "" {
-//		csvFilePath, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-//		if err != nil {
-//			log.Println("Error opening file:", err)
-//			return err
-//		}
-//		defer csvFilePath.Close()
-//
-//		//csvFilePath, err := os.Create(filePath) //new csv file saved to local device
-//
-//		//if err != nil {
-//		//	return err
-//		//}
-//
-//		csvW := csv.NewWriter(csvFilePath) // writer object csvFilePath -> where to write to
-//		newArr := []string{}               // temp storage for string
-//
-//		for _, i := range head {
-//			for j := range i {
-//				x := i[j]
-//				newArr = append(newArr, x) //need to do newline
-//
-//			}
-//		}
-//		_ = csvW.Write(newArr)
-//		newArr = nil
-//
-//		for _, i := range arr {
-//			for j := range i {
-//				x := i[j]
-//				s := strconv.FormatFloat(x, 'f', -1, 64)
-//				newArr = append(newArr, s)
-//			}
-//			_ = csvW.Write(newArr)
-//			newArr = nil //reset array after each loop (after being written to csv)
-//		}
-//
-//		csvW.Flush()
-//
-//		//defer csvFilePath.Close()
-//
-//	}
-//	return nil
-//}
-
 func ToCSV(head [][]string, arr [][]float64, filepath string) error {
 
 	csvFile, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) //new csv file saved to local device
@@ -71,7 +19,7 @@ func ToCSV(head [][]string, arr [][]float64, filepath string) error {
 	for _, i := range head {
 		for j := range i {
 			x := i[j]
-			newArr = append(newArr, x) //need to do newline
+			newArr = append(newArr, x)
 
 		}
 	}
